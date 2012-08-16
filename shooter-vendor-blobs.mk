40254cd2562c0e483056c11a2d2c92d6436456b5
Dorian Snyder <dastin1015@gmail.com>
2012-08-15 18:11:46 -0700

    remove libv8

--- shooter-vendor-blobs.mk
+++ shooter-vendor-blobs.mk
@@ -19,8 +19,7 @@ PRODUCT_COPY_FILES += \
     vendor/htc/shooter/proprietary/camera.default.so:obj/lib/hw/camera.default.so \
     vendor/htc/shooter/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so \
     vendor/htc/shooter/proprietary/libacdbloader.so:obj/lib/libacdbloader.so \
-    vendor/htc/shooter/proprietary/libacdbmapper.so:obj/lib/libacdbmapper.so \
-    vendor/htc/shooter/proprietary/libv8.so:obj/lib/libv8.so
+    vendor/htc/shooter/proprietary/libacdbmapper.so:obj/lib/libacdbmapper.so
     
 # All the blobs necessary for shooter
 PRODUCT_COPY_FILES += \
@@ -81,7 +80,6 @@ PRODUCT_COPY_FILES += \
     vendor/htc/shooter/proprietary/libqdp.so:system/lib/libqdp.so \
     vendor/htc/shooter/proprietary/libqmiservices.so:system/lib/libqmiservices.so \
     vendor/htc/shooter/proprietary/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-    vendor/htc/shooter/proprietary/libv8.so:system/lib/libv8.so \
     vendor/htc/shooter/proprietary/camera.default.so:system/lib/hw/camera.default.so \
     vendor/htc/shooter/proprietary/lights.msm8660.so:system/lib/hw/lights.msm8660.so \
     vendor/htc/shooter/proprietary/sensors.shooter.so:system/lib/hw/sensors.shooter.so \
